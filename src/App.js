6e5b649707cd49ef40d89ac896cc03f4bd485f5f
Pedro <Pedrogor@outlook.com>
2024-05-23 17:52:33 -0300

    Fade in h1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App" style={{ backgroundImage: 'url(https://i.pinimg.com/originals/68/2a/a8/682aa81619f08a3e70882caabc02bf1a.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
      <div className="container">
-        <h1 style={{ textAlign: 'center', color: '#fff' }}>Consulta de Endereço por CEP</h1>
+        <h1 style={{ textAlign: 'center', color: '#fff', animation: 'crescer 0.5s ease-in-out forwards' }}>Consulta de Endereço por CEP</h1>
         <CEPForm />
       </div>
     </div>
