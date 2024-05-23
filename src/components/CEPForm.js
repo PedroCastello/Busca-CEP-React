cafec8262a29e025c874215fec62984e3e8d3492
Pedro <Pedrogor@outlook.com>
2024-05-23 17:41:07 -0300

    link maps

--- src/components/CEPForm.js
+++ src/components/CEPForm.js
@@ -4,11 +4,18 @@ import axios from 'axios';
 const CEPForm = () => {
   const [cep, setCep] = useState('');
   const [endereco, setEndereco] = useState('');
+  const [googleMapsUrl, setGoogleMapsUrl] = useState('');
 
   const buscarEndereco = async () => {
     try {
       const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-      setEndereco(`${response.data.logradouro}, ${response.data.bairro}, ${response.data.localidade} - ${response.data.uf}`);
+      const { logradouro, bairro, localidade, uf } = response.data;
+      setEndereco(`${logradouro}, ${bairro}, ${localidade} - ${uf}`);
+      
+      // Construindo a URL do Google Maps com as coordenadas do CEP
+      const enderecoParaUrl = `${logradouro}, ${bairro}, ${localidade}, ${uf}`;
+      const googleMapsUrl = `https://www.google.com/maps?q=${encodeURIComponent(enderecoParaUrl)}`;
+      setGoogleMapsUrl(googleMapsUrl);
     } catch (error) {
       console.error('Erro ao buscar o endereço:', error);
     }
@@ -30,6 +37,13 @@ const CEPForm = () => {
         placeholder="Digite o CEP" 
       />
       <p>{endereco}</p>
+      {googleMapsUrl && (
+        <p>
+          <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer">
+            Google Maps
+          </a>
+        </p>
+      )}
     </div>
   );
 };
