0bd52c778d92889559e8d8b3186240c2a721bffe
Pedro <Pedrogor@outlook.com>
2024-05-23 18:11:24 -0300

    margins

--- src/components/CEPForm.js
+++ src/components/CEPForm.js
@@ -5,38 +5,54 @@ const CEPForm = () => {
   const [cep, setCep] = useState('');
   const [endereco, setEndereco] = useState('');
   const [googleMapsUrl, setGoogleMapsUrl] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const buscarEndereco = async () => {
     try {
-      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-      const { logradouro, bairro, localidade, uf } = response.data;
-      setEndereco(`${logradouro}, ${bairro}, ${localidade} - ${uf}`);
-      
-      // Construindo a URL do Google Maps com as coordenadas do CEP
-      const enderecoParaUrl = `${logradouro}, ${bairro}, ${localidade}, ${uf}`;
-      const googleMapsUrl = `https://www.google.com/maps?q=${encodeURIComponent(enderecoParaUrl)}`;
-      setGoogleMapsUrl(googleMapsUrl);
+      if (cep.length !== 8) {
+        setErrorMessage('O CEP deve ter 8 dígitos. Por favor, insira um CEP válido.');
+        setEndereco('');
+        setGoogleMapsUrl('');
+      } else {
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+        if (response.data.erro) {
+          setErrorMessage('CEP não encontrado. Por favor, insira um CEP válido.');
+          setEndereco('');
+          setGoogleMapsUrl('');
+        } else {
+          const { logradouro, bairro, localidade, uf } = response.data;
+          setEndereco(`${logradouro}, ${bairro}, ${localidade} - ${uf}`);
+          
+          // Construindo a URL do Google Maps com as coordenadas do CEP
+          const enderecoParaUrl = `${logradouro}, ${bairro}, ${localidade}, ${uf}`;
+          const googleMapsUrl = `https://www.google.com/maps?q=${encodeURIComponent(enderecoParaUrl)}`;
+          setGoogleMapsUrl(googleMapsUrl);
+          setErrorMessage('');
+        }
+      }
     } catch (error) {
       console.error('Erro ao buscar o endereço:', error);
     }
   };
 
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      buscarEndereco();
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    buscarEndereco();
   };
 
   return (
     <div>
-      <input 
-        type="text" 
-        value={cep} 
-        onChange={(e) => setCep(e.target.value)} 
-        onKeyPress={handleKeyPress} // Chamar a função ao pressionar "Enter"
-        placeholder="Digite o CEP" 
-      />
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          value={cep} 
+          onChange={(e) => setCep(e.target.value)} 
+          placeholder="Digite o CEP" 
+        />
+        <button type="submit">Buscar</button>
+      </form>
       <p>{endereco}</p>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
       {googleMapsUrl && (
         <p>
           <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer">
